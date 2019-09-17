bf5308af13257e9303ce912f63389ca2fd765234
Vikar007 <vikar_2000@mail.ru>
2019-09-17 22:33:12 +0500

    new commit

--- src/Ball/index.js
+++ src/Ball/index.js
@@ -2,6 +2,7 @@ import React from 'react';
 import styles from './index.module.css';
 const Ball = ({ val_x, val_y }) =>
   <div className={styles.Box} style={{ left: val_x, top: val_y }} >
+    <div className={styles.Light}></div>
   </div>
 
 
--- src/app/index.js
+++ src/app/index.js
@@ -1,4 +1,5 @@
 import React from 'react';
+import ReactDOM from 'react-dom'
 import './index.css';
 import Racket from '../Racket/index.js';
 import Ball from '../Ball/index.js';
@@ -7,50 +8,59 @@ class Arc extends React.Component {
   constructor(props) {
     super(props)
 
-
+    this.timer = null;
+    this.rect = null;
 
     this.state = {
-      racket_x: 20,
-      ball_x: 250,
-      ball_y: 40,
-      gor: 'left',
-      ver: 'up'
+      racket_x: 40,
+      ball_x: 260,
+      ball_y: 640,
+      step_x: 4,
+      step_y: -4,
+      start_stop: false
     }
 
-    // this.onKeyPress = this.onKeyPress.bind(this);
+    this.onStop = this.onStop.bind(this);
+    this.onStart = this.onStart.bind(this);
   }
-  onMuve() {
-    const { ball_x, ball_y, gor, ver } = this.state;
-    const step_x = 10;
-    const step_y = 10;
 
-    if (ball_x <= 470 && gor === 'right') {
-      this.setState({ ball_x: ball_x + step_x })
-    }
-    else { this.setState({ gor: 'left' }) };
-
-    if (ball_x >= 10 && gor === 'left') {
-      this.setState({ ball_x: ball_x - step_x })
-    }
-    else { this.setState({ gor: 'right' }) };
-
-    console.log(gor);
-    console.log(ball_x);
-
-
-    if (ball_y <= 400 && ver === 'down') {
-      this.setState({ ball_y: ball_y + step_y })
-    }
-    else { this.setState({ ver: 'up' }) };
-
-    if (ball_y >= 10 && ver === 'up') {
-      this.setState({ ball_y: ball_y - step_y })
-    }
-    else { this.setState({ ver: 'down' }) };
-
-    console.log(gor);
-    console.log(ball_x);
+  componentDidMount() {
+    const element = document.getElementById('main_div');
+    this.rect = element.getBoundingClientRect()
+    console.log(this.rect);
   }
+
+  onBallMove() {
+
+    let step = 4;
+    const { ball_x, ball_y, step_x, step_y, racket_x } = this.state;
+
+    if (ball_x >= 472) { this.setState({ step_x: -step }) };
+    if (ball_x <= 5) { this.setState({ step_x: step }) };
+    this.setState({ ball_x: ball_x + step_x })
+
+    const reflected = (ball_y >= 630 && ball_x >= racket_x && ball_x <= racket_x + 100);
+
+    if (reflected) { this.setState({ step_y: -step }) }
+
+    //Если не смогли отразить шарик, то...
+    if (ball_y > 660 && !reflected) {
+      clearInterval(this.timer);
+      this.setState({ start_stop: false })
+    };
+    if (ball_y <= 5) { this.setState({ step_y: step }) };
+    this.setState({ ball_y: ball_y + step_y })
+  }
+
+  onMouseMove = e => {
+    const mouse_x = e.pageX;
+    const { rect } = this;
+    if ((mouse_x > rect.left + 50) && (mouse_x < rect.left + 450)) {
+      this.setState({ racket_x: mouse_x - rect.left - 50 });
+    }
+  }
+
+
   onKeyDown = e => {
     const step = 20;
     const { racket_x } = this.state;
@@ -59,32 +69,63 @@ class Arc extends React.Component {
     }
     if (e.keyCode === 39 && racket_x <= 400 - step) {
       this.setState({ racket_x: racket_x + step })
+      clearInterval(this.timer);
     }
     // console.log(racket_x);
   }
 
-  componentDidMount() {
 
-    this.timer = setInterval(
-      () => this.onMuve(),
-      50
-    );
+
+  onStart() {
+    if (!this.state.start_stop) {
+      this.setState({
+        racket_x: 40,
+        ball_x: 260,
+        ball_y: 640,
+        step_x: -4,
+        step_y: -4,
+        start_stop: true
+      })
+
+      this.timer = setInterval(
+        () => {
+          this.onBallMove();
+        },
+        20
+      );
+    };
   }
 
   componentWillUnmount() {
     clearInterval(this.timer);
   }
 
+  onStop() {
+    clearInterval(this.timer);
+    this.setState({ start_stop: false })
+  }
+
   render() {
     const { racket_x } = this.state;
     const { ball_x, ball_y } = this.state;
+    // console.log('Значение rect - ', this.rect);
     return (
-      <div className="Box" onKeyDown={this.onKeyDown} tabIndex="0" >
-        <Ball val_x={ball_x} val_y={ball_y} />
-        <Racket value={racket_x} />
+      <div className='Warp'>
+        <div id='main_div' className="Box" onKeyDown={this.onKeyDown} tabIndex="0" onMouseMove={this.onMouseMove} >
 
-      </div>
-      // <button onClick={this.onMuve()}>click</button>
+          <div className={'Brix'} style={{ left: 0 }}></div>
+          <div className={'Brix'} style={{ left: 100 }}></div>
+          <div className={'Brix'} style={{ left: 200 }}></div>
+          <div className={'Brix'} style={{ left: 300 }}></div>
+          <div className={'Brix'} style={{ left: 400 }}></div>
+          <Ball val_x={ball_x} val_y={ball_y} />
+          <Racket value={racket_x} />
+        </div>
+        <div>
+          <button className='Btn' style={{ left: 160 }} onClick={this.onStart}>Старт</button>
+          <button className='Btn' style={{ left: 260 }} onClick={this.onStop}>Стоп</button>
+        </div>
+      </div >
     );
   }
 }
